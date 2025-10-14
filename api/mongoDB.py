3a13c01f5d17d3fcd661897842382513fb9e9403
JasonP1 <jasonstudent64549@gmail.com>
2025-10-14 10:58:36 -0400

    Updated MongoDB integration by using .env, also created .env and .gitignore files

--- api/mongoDB.py
+++ api/mongoDB.py
@@ -1,16 +1,21 @@
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
-import pandas as pd
+import os
+import time
 
 # Create a new client and connect to the server
 def upload(data):
-    password = "Yg675QwLtUN6YsVi"
-    uri = "mongodb+srv://myuser:" + password + "@bbp.lh2bp4a.mongodb.net/?retryWrites=true&w=majority&appName=bbp"
+    username = os.getenv('MONGO_USR')
+    password = os.getenv('MONGO_PSS')
+    domain = os.getenv('MONGO_DOMAIN')
+    uri = "mongodb+srv://" + username + ":" + password + domain + "/?retryWrites=true&w=majority&appName=bbp"
     client = MongoClient(uri, server_api=ServerApi('1'))
     db = client['WaterQualityData']
     collection = db['clean_data']
+
+    # waits 2 seconds in case more than one user uploads data at the same time
+    time.sleep(2)
     # existing entries are cleared before insertion
     if (collection.count_documents({}) > 0):
         collection.delete_many({})
-    else:
-        collection.insert_many(data)
+    collection.insert_many(data)
